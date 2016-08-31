0010064cf12e09d6f93a2b867f901e09b6440b1c
Kim Nguyen <zktnguyen@ucdavis.edu>
2016-08-31 13:46:26 -0700

    beginning

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by keem on 8/31/2016.
+ */
+
+
+function startGame() {
+    document.getElementById("start").style.visiblity = 'hidden';
+
+    if (document.getElementById("start").innerHTML === "START") {
+        document.getElementById("start").style.width = "50px";
+        document.getElementById("start").innerHTML = "RESTART";
+    }
+
+    beginGame();
+}
+
+function beginGame() {
+
+}
