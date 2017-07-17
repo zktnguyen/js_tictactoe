4e7c26aa1fb3f18e224d6a0e4cff5d4db0444f92
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-07-16 18:36:59 -0700

    currently works without an AI

--- js/script.js
+++ js/script.js
@@ -7,15 +7,38 @@
       yourID = $("#your-id"),
       computerID = $("#computer-id"),
       allBoxes = $(".box"),
+      winners = $("#list-winners"),
       myPlayer = "X",
-      computerPlayer = "O";
+      computerPlayer = "O",
+      currentPlayer = "O",
+      turn = 0,
+      placed = [" ", " ", " ",
+                " ", " ", " ", 
+                " ", " ", " ",];
     // choose player
-    function resetGame() {
+    function resetGame(gameResult) {
+
+      if (gameResult){
+        var args = Array.from(arguments);
+        var winner = args[1];
+        if (winner === "tie"){
+          winners.append("<li class='tie-text'>Tie</li>");
+        }
+        else {
+          var html = winner === myPlayer ? "<li class='your-text'>You won</li>" 
+                                       : "<li class='computer-text'>Computer won</li>";
+          winners.append(html);
+        }
+      }
       allBoxes.html("&nbsp;");
+      for (var i = 0; i < 9; i++){
+        placed[i] = " ";
+      }
+      turn = 0;
     }
 
     playerX.click(function(){
-      resetGame();
+      resetGame(false);
       myPlayer = "X";
       computerPlayer = "O";
       if (playerX.hasClass("btn-success")){
@@ -32,7 +55,7 @@
     });
 
     playerO.click(function(){
-      resetGame();
+      resetGame(false);
       myPlayer = "O";
       computerPlayer = "X";
       if (playerO.hasClass("btn-success")){
@@ -46,6 +69,54 @@
 
       yourID.html("You are O");
       computerID.html("Computer is X");
+    });
+    function checkWinner(player) {
+      var winningCombo = [
+        [0,1,2],[3,4,5],[6,7,8],
+          [0,4,8],[2,4,6],
+          [0,3,6],[1,4,7],[2,5,8]
+      ];
+      for (var i = 0; i < winningCombo.length; i++){
+
+
+          var a = winningCombo[i][0];
+          var b = winningCombo[i][1];
+          var c = winningCombo[i][2];
+
+        if ((player === placed[a] &&
+            player === placed[b] &&
+            player === placed[c])){
+            
+            resetGame(true, player);
+            break;
+        }
+
+          else if (turn === 9){
+            resetGame(true, "tie");
+            break;
+        }
+
+      }
+    }
+    function makeMove(player, boxNum) {
+      var box = $("#box"+ boxNum);
+      if (box.html() === "&nbsp;"){
+        box.html(player);
+        placed[boxNum] = player;
+        turn++;
+        currentPlayer = currentPlayer === "O" ? "X" : "O";
+        checkWinner(placed[boxNum]);
+      }
+      else {
+        console.log("Please select another box");
+      }
+
+    } 
+
+    allBoxes.click(function(){
+      var boxNum = $(this).attr('id').charAt(3);
+      makeMove(currentPlayer, boxNum);
+
     });
   });
 
