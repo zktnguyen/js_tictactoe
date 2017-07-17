336f71222bdae0f0606653274c05c92db38f1e08
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-07-16 17:48:20 -0700

    changed the format of the grid, added functionality through javascript for the basic buttons.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,52 @@
 (function($) {
   $(function(){
+    // connect all of the squares of the tic tac toe board
+    // create AI
+    var playerX = $("#playerX"),
+      playerO = $("#playerO"),
+      yourID = $("#your-id"),
+      computerID = $("#computer-id"),
+      allBoxes = $(".box"),
+      myPlayer = "X",
+      computerPlayer = "O";
+    // choose player
+    function resetGame() {
+      allBoxes.html("&nbsp;");
+    }
 
+    playerX.click(function(){
+      resetGame();
+      myPlayer = "X";
+      computerPlayer = "O";
+      if (playerX.hasClass("btn-success")){
+        playerX.removeClass("btn-success").addClass("btn-danger");
+        playerO.removeClass("btn-danger").addClass("btn-success");
+      }
+      else {
+        playerO.removeClass("btn-success").addClass("btn-danger");
+        playerX.removeClass("btn-danger").addClass("btn-success");
+      }
+
+      yourID.html("You are X");
+      computerID.html("Computer is O");
+    });
+
+    playerO.click(function(){
+      resetGame();
+      myPlayer = "O";
+      computerPlayer = "X";
+      if (playerO.hasClass("btn-success")){
+        playerO.removeClass("btn-success").addClass("btn-danger");
+        playerX.removeClass("btn-danger").addClass("btn-success");
+      }
+      else {
+        playerX.removeClass("btn-success").addClass("btn-danger");
+        playerO.removeClass("btn-danger").addClass("btn-success");
+      }
+
+      yourID.html("You are O");
+      computerID.html("Computer is X");
+    });
   });
 
 })(jQuery);
